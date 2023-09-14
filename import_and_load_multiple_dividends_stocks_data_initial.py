8b344eddfb67c3bfd5aab1cc9928e1f5b59550af
Tharindu Abeysinghe <tha8761@gmail.com>
2023-09-14 17:42:38 -0500

    Update to insert dividends data
    Create new history object and add two new columns to the dividends data table

--- import_and_load_multiple_dividends_stocks_data_initial.py
+++ import_and_load_multiple_dividends_stocks_data_initial.py
@@ -13,6 +13,7 @@ tickers = ['AAPL', 'MSFT', 'SPG', 'PEP', 'F', 'SHW', 'T', 'ORCL', 'V', 'BAC', 'U
 # Today's date
 today = datetime.today().strftime('%Y-%m-%d')
 
+# Import data using yahoo finances API for each ticker and load to a dataframe
 dataframes = []
 for ticker_name in tickers:
     ticker = yf.Ticker(ticker_name)
@@ -25,11 +26,22 @@ for ticker_name in tickers:
     stock_data = stock_data.reset_index()
     stock_data['daily_return'] = (stock_data['Close'] / stock_data['Close'].shift(1)) - 1
     stock_data['daily_return'].fillna(0,inplace=True)
-    dataframes.append(stock_data)
-dividends_stocks_data = pd.concat(dataframes, ignore_index=True)
+    stock_data['Date'] = stock_data['Date'].dt.strftime('%Y-%m-%d')
+    div_data = ticker.history(start=first_trade_date, end=today)
+    div_data = div_data[['Dividends', 'Stock Splits']].reset_index()
+    div_data['Date'] = pd.to_datetime(div_data['Date']).dt.date
+    div_data['Date'] = div_data['Date'].astype(str)
+    stock_data['Date'] = stock_data['Date'].astype(str)
+    stock_data_merged = stock_data.merge(div_data, left_on='Date', right_on='Date',how='left')
+    dataframes.append(stock_data_merged)
+multiple_stocks_data = pd.concat(dataframes, ignore_index=True)
 
-dividends_stocks_data.columns = [column_name.lower() for column_name in dividends_stocks_data.columns]
-
+# Update column names and data types 
+multiple_stocks_data['dividends'] = pd.to_numeric(multiple_stocks_data['Dividends'], errors='coerce')
+multiple_stocks_data['stock_splits'] = pd.to_numeric(multiple_stocks_data['Stock Splits'], errors='coerce')
+multiple_stocks_data.drop(['Stock Splits', 'Dividends'], axis=1, inplace=True)
+multiple_stocks_data['Date']=pd.to_datetime(multiple_stocks_data['Date'],format='%Y-%m-%d')
+multiple_stocks_data.columns = [column_name.lower() for column_name in multiple_stocks_data.columns]
 
 #### INSERT TO PostgreSQL database ######
 
@@ -52,7 +64,9 @@ cur.execute('''CREATE TABLE IF NOT EXISTS dividends_stocks_daily
                 Adj_Close FLOAT NOT NULL,
                 Volume BIGINT NOT NULL,
                 Ticker VARCHAR(255),
-                Daily_return FLOAT
+                Daily_return FLOAT,
+                Dividends FLOAT,
+                Stock_splits FLOAT
                 );''')
                
 print("Table created successfully")
@@ -60,4 +74,4 @@ conn.close()
 
 # Create an engine and load the data from the dataframe to postgresql database
 engine = create_engine('postgresql+psycopg2://tharinduabeysinghe:#####@localhost/stocks')
-dividends_stocks_data.to_sql('dividends_stocks_daily', engine, if_exists='append', index=False)
+multiple_stocks_data.to_sql('dividends_stocks_daily', engine, if_exists='append', index=False)
